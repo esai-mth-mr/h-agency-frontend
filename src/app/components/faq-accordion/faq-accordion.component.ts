4b407f3872dd647bdd138736f345618eeccc7a5f
undyingkevin <williamadams19901111@gmail.com>
2024-12-13 11:01:34 -0500

    final frontend

--- src/app/components/faq-accordion/faq-accordion.component.ts
+++ src/app/components/faq-accordion/faq-accordion.component.ts
@@ -1,9 +1,9 @@
 import {ChangeDetectionStrategy, Component, ɵɵInputTransformsFeature, signal} from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material.module';
 @Component({
   selector: 'app-faq-accordion',
-  imports: [MaterialModule],
+  imports: [MaterialModule, CommonModule],
   templateUrl: './faq-accordion.component.html',
   styleUrl: './faq-accordion.component.css'
 })
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import {ChangeDetectionStrategy, Component, ɵɵInputTransformsFeature, signal} from '@angular/core';
-
+import { IndustryCarouselComponent } from '../industry-carousel/industry-carousel.component';
+import { FaqAccordionComponent } from '../faq-accordion/faq-accordion.component';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,  
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [IndustryCarouselComponent, FaqAccordionComponent],  
 
 })
 export class LandingComponent {
